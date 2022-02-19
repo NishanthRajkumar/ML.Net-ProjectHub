599b95da6c2a606885d86d600f35de92b60f3d2b
Nishanth <rajkumarnishanth@gmail.com>
2022-02-19 21:23:21 +0530

    My first ML.Net App; Sentiment model using basic 'yelp reviews' data set

---
+++ MyFirstMLApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyFirstMLApp;
+
+//Load sample data
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = @"My go to place from now on",
+};
+
+//Load model and predict output
+var result = SentimentModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+string sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
